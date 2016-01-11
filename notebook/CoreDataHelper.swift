b97c31bf485774919bfc67ecfe7a595b089ef077
manishaparmar2013 <manishaparmar.net@gmail.com>
2016-01-11 11:20:41 +1100

    test

---
+++ notebook/CoreDataHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelper.swift
+//  notebook
+//
+//  Created by Manisha Parmar on 11/01/2016.
+//  Copyright © 2016 Manisha Parmar. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataHelper: NSObject {
+
+}
